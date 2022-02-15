ffe8cf097649b8ed979c8517b6cee13eccaf7809
Mehmet Dikici <mehmetdi@gmail.com>
2022-02-15 10:25:37 +0200

    Java test file

---
+++ test.java
@@ -0,0 +1,16 @@
+public class Test {
+    public static void main(String[] args) {
+        byte byteVariable = 127;
+        short shortVariable = 32767;
+        int intVariable = 125165;
+        long longVariable = 92233720;
+        float floatVariable = 10.12f;
+        double doubleVariable = 11214.50;
+        char charVariable = 'M';
+        boolean booleanVariable = true;
+
+        System.out.print("byte example: " + byteVariable + "\nshort example: " + shortVariable + "\nint example: " + intVariable +
+                "\nlong example: " + longVariable + "\nfloat example: " + floatVariable + "\ndouble example: " + doubleVariable +
+                "\nchar example: " + charVariable + "\nboolean example: " + booleanVariable);
+    }
+}
